2ed55308099b14b3419cc12943f98033d30941cb
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-08-13 16:20:09 +0300

    13.08.2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from 'src/app/models/product';
 
 @Component({
@@ -9,10 +11,19 @@ import { Product } from 'src/app/models/product';
 export class ProductComponent implements OnInit {
 
   products:Product[] = []
-
-  constructor() { }
+  apiUrl="https://localhost:44361/api/products/getall"
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe((response)=>{
+      this.products=response.data
+    })
   }
 
 }
