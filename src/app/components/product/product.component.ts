a273677d6d09434ca2edd38d37f04139e4432e65
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-08-13 11:46:45 +0300

    13.08.2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,13 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  product: any = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 1 }
-  product1: any = { productId: 2, productName: "laptop", categoryId: 1, unitPrice: 1 }
-  product2: any = { productId: 3, productName: "mouse", categoryId: 1, unitPrice: 1 }
-  product3: any = { productId: 4, productName: "keyboard", categoryId: 1, unitPrice: 1 }
-  product4: any = { productId: 5, productName: "camera", categoryId: 1, unitPrice: 1 }
 
-  products = [this.product, this.product1, this.product2, this.product3, this.product4]
+  products:Product[] = []
 
   constructor() { }
 
